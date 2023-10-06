14157a7e9933dbf8f89a6d9c364f9383b49ed540
AceDemo1 <lawal641@gmail.com>
2023-10-06 14:05:11 -0700

    lowercase aphabet detection

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    for t in c:
-        if 'a' <= t <= 'z':
-            return (True)
-        else:
-            return (False)
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
+
