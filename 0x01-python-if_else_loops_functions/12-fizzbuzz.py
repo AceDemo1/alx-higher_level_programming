a83e3da2f60ee5c881e618ba059da65cd0293c4b
AceDemo1 <lawal641@gmail.com>
2023-10-07 03:33:22 -0700

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num range(1, 101):
+    for num in range(1, 101):
         if num % 3 = 0:
-            print("Fizz ", end ='')
+            print("Fizz ", end='')
         elif num % 5 = 0:
-            print("Buzz ", end ='')
+            print("Buzz ", end='')
         elif num % 3 = 0 and num % 5 = 0:
             print("FizzBuzz")
         print(num, end='')
