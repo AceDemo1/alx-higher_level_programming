8c3a0e6c80367d48db6451af47febbae6c0085b4
AceDemo1 <lawal641@gmail.com>
2023-10-07 05:14:55 -0700

    alpha alternating

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for alpha in range(ord('z'), ord('a'), -1):
+    if alpha % 2 != 0:
+        conv = alpha - 32
+    else:
+        conv = 0
+    print("{}".format(chr(ord(alpha) - conv))
