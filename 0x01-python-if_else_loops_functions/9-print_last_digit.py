416c0505d217e071d8a53af543cc818aa495355e
AceDemo1 <lawal641@gmail.com>
2023-10-07 03:00:45 -0700

    a function that prints a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         num = (-1 * number) % 10
-        print(-num, end='')
+        return(-num, end='')
     elif number >= 0:
         num = number % 10
-        print(num, end='')
+        return(num, end='')
