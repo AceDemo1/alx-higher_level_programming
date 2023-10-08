15a9c9ee31af5f4e45a8170a2e71f480cb9bf479
AceDemo1 <lawal641@gmail.com>
2023-10-08 07:12:35 -0700

    alpha alternating capital

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for alpha in range(ord('z'), ord('a'), -1):
-    if alpha % 2 != 0:
+for alpha in range(ord('z'), ord('a') - 1, -1):
+    if alpha % 2 == 0:
         conv = alpha - 32
     else:
         conv = 0
