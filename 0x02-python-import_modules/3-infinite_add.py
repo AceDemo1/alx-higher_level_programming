a3dc26e0fe672ff5da82ac3c4ec10a4aa089dfb6
AceDemo1 <lawal641@gmail.com>
2023-10-10 07:27:31 -0700

    sum

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,8 @@ if __name__ == "__main__"
     sum = 0
     num = len(sys.argv)
     if num < 2:
-        print(0)
+        print("0")
     else: 
         for i in range(1, num):
             sum += int(argv[i])
-            print(sum)
+            print("{}".format(sum))
