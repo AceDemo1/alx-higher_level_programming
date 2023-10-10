fcb70b415355eb521f86cbf56841dcdf04a2a7b3
AceDemo1 <lawal641@gmail.com>
2023-10-10 00:46:10 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,10 @@ if __name__ == "__main__":
     numargs = len(args)
     if numargs == 1:
         print("0 arguments.")
-    elif numargs == 2:
-        print("1 argument:")
-    elif numargs > 2:
-        print("{} arguments:".format(numargs - 1))
-    for i in range(1, numargs):
-        print("{}: {}".format(i, args[i])
+    else:
+        if numargs == 2:
+            print("1 argument:")
+        elif numargs > 2:
+            print("{} arguments:".format(numargs - 1))
+        for i in range(1, numargs):
+            print("{}: {}".format(i, args[i])
