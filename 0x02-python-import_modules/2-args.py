ee9a24c22f9002983305e03ce61352ffa186999b
AceDemo1 <lawal641@gmail.com>
2023-10-10 00:39:16 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-
-# Remove the script name from the arguments
-args = argv[1:]
-
-# Get the number of arguments
-num_args = len(args)
-
-# Print the number of arguments
-print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''), end='')
-
-# Print a period if there are no arguments
-if num_args == 0:
-    print('.')
-else:
-    print('')
-
-# Print the list of arguments
-for i, arg in enumerate(args, 1):
-    print("{}: {}".format(i, arg))
-
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    numargs = len(args)
+    if numargs == 1:
+        print("0 arguments.")
+    elif numargs == 2:
+        print("1 argument:")
+    elif numargs > 2:
+        print("{} arguments:".format(numargs - 1))
+    for i in range(1, numargs):
+        print("{}: {}".format(i, args[i])
