f9137a22612486efc9f20661bf8e0c79226caaba
AceDemo1 <lawal641@gmail.com>
2023-10-10 05:32:01 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     form sys import argv
     numargs = len(argv)
-    if numargs == 0:
-        print("{} arguments.".format(numargs))
-    elif numargs == 1:
+    if numargs == 1:
         print("{} arguments.".format(numargs - 1))
     elif numargs == 2:
         print("{} argument.".format(numargs - 1))
