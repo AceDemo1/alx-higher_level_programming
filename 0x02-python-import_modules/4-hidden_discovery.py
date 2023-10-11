edc95d0a28ea9afdbc15c7362df97fc2576e2ba3
AceDemo1 <lawal641@gmail.com>
2023-10-11 12:12:02 -0700

    dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
         import hidden_4
         names = dir(hidden_4)
-        for i in names
+        for i in names:
                 if i[:2] == "__":
                         continue
                 print("{:s}".format(i))
