235244cda83ae9836e6bfdabe81f056944d8493c
AceDemo1 <lawal641@gmail.com>
2023-10-14 15:16:28 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     maxt = []
     for i in matrix:
-        res = list(map(lambda x: x ** 2, i)
+        res = list(map(lambda x: x ** 2, i))
         maxt.append(res)
     return (maxt)
