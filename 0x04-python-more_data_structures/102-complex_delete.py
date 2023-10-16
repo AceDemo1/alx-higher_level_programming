1113e1bb566d249d24c0ab67703f399234bbdf9b
AceDemo1 <lawal641@gmail.com>
2023-10-16 10:14:37 -0700

    del based on value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-        dele = []
-        for i in a_dictionary:
-                if i == value:
-                        dele.append(i)
-        for i in dele:
-                del a_dictionary[i]
-        return (a_dictionary)
+    dele = []
+    for i in a_dictionary:
+        if a_dictionary[i] == value:
+            dele.append(i)
+    for i in dele:
+        del a_dictionary[i]
+    return (a_dictionary)
