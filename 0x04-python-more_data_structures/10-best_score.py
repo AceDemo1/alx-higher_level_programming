d840d4ad762ad698eec4c9b94bd1ce936707f3f4
AceDemo1 <lawal641@gmail.com>
2023-10-15 17:48:25 -0700

    max num

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,5 +6,5 @@ def best_score(a_dictionary):
         num = list(a_dictionary.keys())[0]
         for i in a_dictionary:
             if a_dictionary[i] > a_dictionary[num]:
-                num = i                    
+                num = i
         return (num)
