f747e1047086a80bb1bd60da03980ef214055f46
AceDemo1 <lawal641@gmail.com>
2023-10-29 14:28:34 -0700

    Instantiation with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,4 +4,5 @@
 class Square:
     """private instance"""
     def __init__(self, size):
-        self.__size == size
+        """initialize the class withe the size instance"""
+        self.__size
