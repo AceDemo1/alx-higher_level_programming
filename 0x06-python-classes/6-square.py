e0b555bd4983e841226dd5b7bf311c4fed01a2e4
AceDemo1 <lawal641@gmail.com>
2023-10-30 11:56:59 -0700

    position instance

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """class square"""
 
-
 class Square:
     """private instance"""
-
     def __init__(self, size=0, position=(0, 0)):
-        """initialize the class withe the size instance"""
+        """initialize the class with the size and position attributes"""
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -24,28 +23,28 @@ class Square:
 
     @property
     def position(self):
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if type(position) is not tuple or len(position) != 2:
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        for el in position:
+        for el in value:
             if el < 0 or type(el) is not int:
                 raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
     def area(self):
-        """to cal. the area"""
-        return (self.__size ** 2)
+        """calculate the area"""
+        return self.__size ** 2
 
     def my_print(self):
         if self.__size == 0:
             print()
         else:
-            print("\n" * self.__position[1])
+            print('\n' * self.__position[1], end='')
             for i in range(self.__size):
-                print(" " * self.__position[0], end='')
+                print(' ' * self.__position[0], end='')
                 for j in range(self.__size):
-                    print("#", end='')
+                    print('#', end='')
                 print()
