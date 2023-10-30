a886b4760e7cd06a788d8b20b9480f78f9362241
AceDemo1 <lawal641@gmail.com>
2023-10-30 13:04:29 -0700

    position instance

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,47 +1,42 @@
 #!/usr/bin/python3
 """class square"""
 
-
 class Square:
     """private instance"""
-
     def __init__(self, size=0, position=(0, 0)):
-        """initialize the class withe the size instance"""
+        """initialize the class with the size and position attributes"""
         self.size = size
         self.position = position
 
-    def __str__(self):
-        self.my_print()
-
     @property
     def size(self):
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('size must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('size must be >= 0')
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple):
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
+        for el in value:
+            if el < 0 or type(el) is not int:
+                raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
     def area(self):
-        """to cal. the area"""
-        return (self.__size ** 2)
+        """calculate the area"""
+        return self.__size ** 2
 
     def my_print(self):
         if self.__size == 0:
@@ -49,7 +44,8 @@ class Square:
         else:
             print('\n' * self.__position[1], end='')
             for i in range(self.__size):
-                print(" " * self.__position[0], end='')
+                print(' ' * self.__position[0], end='')
                 for j in range(self.__size):
-                    print("#", end='')
+                    print('#', end='')
                 print()
+
