846017e0dd7b4d41b393e4e10e58ed9df626ec5f
AceDemo1 <lawal641@gmail.com>
2023-10-29 21:23:13 -0700

    Instantiation with size and raising errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square:
 def __init__(self, size=0):
         """initialize the class withe the size instance"""
         if type(size) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = size
