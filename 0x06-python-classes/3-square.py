b65b3b5e5ffd3d3f766242607b5ddecb388b6176
AceDemo1 <lawal641@gmail.com>
2023-10-29 21:49:53 -0700

    sqr with area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,6 +14,6 @@ class Square:
             raise ValueError('size must be >= 0')
         else:
             self.__size = size
-        def area(self):
-            """to cal. the area"""
-            return (self.__size ** 2)
+    def area(self):
+        """to cal. the area"""
+        return (self.__size ** 2)
