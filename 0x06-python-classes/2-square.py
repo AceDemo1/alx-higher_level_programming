a76e4d6099c5719db4b6c0429f8bd746bc2d19c4
AceDemo1 <lawal641@gmail.com>
2023-10-29 21:18:56 -0700

    Instantiation with size and raising errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""class square"""
+""" class Square that defines a square"""
 
 
 class Square:
-    """private instance"""
+    """ class Square that defines a square"""
 
-def __init__(self, size):
-        """initialize the class withe the size instance"""
+    def __init__(self, size=0):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
         if type(size) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
-            self.__size = size
+            self.__size = size  #: size of the square
