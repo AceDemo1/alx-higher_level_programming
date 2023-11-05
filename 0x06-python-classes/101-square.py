8c477dc4690f65b934df752d667004df44a662eb
AceDemo1 <lawal641@gmail.com>
2023-11-05 08:01:44 -0800

    update

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -45,11 +45,11 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
+        if self.__size > 0:
             for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
                 print(' ' * self.__position[0], end='')
                 print('#' * self.__size)
+        else:
+            print()
