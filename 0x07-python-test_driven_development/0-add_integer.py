3538191fbeae82db68962b60394a1042f1f06fcb
AceDemo1 <lawal641@gmail.com>
2024-04-24 23:34:23 -0700

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,10 +7,11 @@ The 0-add_integer module supplies one function, add_integer(a, b).
 
 def add_integer(a, b=98):
     """Return the addition of two numbers."""
+    
+    if type(a) is not int or type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int or type(b) is not float:
+        raise TypeError('b must be an integer')
     a = int(a)
     b = int(b)
-    if type(a) is not int:
-        raise TypeError('a must be an integer')
-    if type(b) is not int:
-        raise TypeError('b must be an integer')
     return a + b
