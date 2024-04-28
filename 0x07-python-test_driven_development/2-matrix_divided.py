29a8389486d73b8fdd1f55c9d2cd5553b50715d1
AceDemo1 <lawal641@gmail.com>
2024-04-28 12:19:49 -0700

    add

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,7 +4,7 @@ def matrix_divided(matrix, div):
     for i in range(len(matrix)):
         l = []
         for k in range(len(matrix[i])):
-            l += [matrix[k][i] / div]
+            l += [round(matrix[k][i] / div, 2)]
         j += [l]
     return j
             
