07f81fd3cf46a034377885f0adb7446183046a6b
AceDemo1 <lawal641@gmail.com>
2024-04-29 07:07:37 -0700

    text modified

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    sym = ['.', '?', ':']
+    for i in test:
+        if i is in sym:
+            print('\n\n', end='')
+            continue
+        print(i, end='')
