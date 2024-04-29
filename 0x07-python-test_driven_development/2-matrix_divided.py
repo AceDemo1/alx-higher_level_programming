90ddcaf1d06503f158258a41c35f56ff2d6d3d20
AceDemo1 <lawal641@gmail.com>
2024-04-29 04:31:48 -0700

    matrix div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,7 +26,7 @@ def matrix_divided(matrix, div):
         matx = []
         for k in range(len(matrix[i])):
             if type(matrix[i][k]) is not int and \
-                type(matrix[i][k]) is not float:
+            type(matrix[i][k]) is not float:
                 raise TypeError(m)
             matx += [round(matrix[i][k] / div, 2)]
         j += [matx]
