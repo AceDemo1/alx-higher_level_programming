7fa49ab9e8ddaf54f23e46204e3cb8c6a469ab0c
AceDemo1 <lawal641@gmail.com>
2024-04-29 04:29:13 -0700

    matrix div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,8 +7,10 @@ The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 
 def matrix_divided(matrix, div):
     """Divides all elements in the matrix by div"""
+    m = 'matrix must be a matrix (list of lists) of integers/floats'
+    m2 = "Each row of the matrix must have the same size"
     if type(matrix) is not list:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError(m)
     if type(div) is not int and type(div) is not float:
         raise TypeError('div must be a number')
     if div == 0:
@@ -16,17 +18,17 @@ def matrix_divided(matrix, div):
     j = []
     for i in range(len(matrix)):
         if type(matrix[i]) is not list:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError(m)
         if i == 0:
             count = len(matrix[i])
         elif count != len(matrix[i]):
-            raise TypeError("Each row of the matrix must have the same size")
-        l = []
+            raise TypeError(m2)
+        matx = []
         for k in range(len(matrix[i])):
             if type(matrix[i][k]) is not int and type(matrix[i][k]) is not float:
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-            l += [round(matrix[i][k] / div, 2)]
-        j += [l]
+                raise TypeError(m)
+            matx += [round(matrix[i][k] / div, 2)]
+        j += [matx]
     return j
             
 
