a00026f9848015fff19a4c54dae49f9dda78f003
AceDemo1 <lawal641@gmail.com>
2024-04-30 09:22:40 -0700

    mul matirx

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -31,11 +31,11 @@ def matrix_mul(m_a, m_b):
     for i in range(len(m_a)):
         if type(m_a[i]) is not list:
             raise TypeError('m_a must be a list of lists')
+    if type(m_b) is not list:
+       raise TypeError('m_b must be a list')
     for k in range(len(m_b)):
         if type(m_b[k]) is not list:
             raise TypeError('m_b must be a list of lists')
-    if type(m_b) is not list:
-       raise TypeError('m_b must be a list')
     if not m_a or not m_a[0]:
         raise ValueError("m_a can't be empty")
     if not m_b or not m_b[0]:
