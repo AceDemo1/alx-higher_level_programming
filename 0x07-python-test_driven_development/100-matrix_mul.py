48a282c700148ce10d3971a4e6f0246ace12101c
AceDemo1 <lawal641@gmail.com>
2024-04-30 08:55:03 -0700

    mul matirx

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -38,18 +38,20 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
     re = []
+    lea = len(m_a[0])
     for i in range(len(m_a)):
+        if len(m_a[i]) != lea:
+            raise TypeError("each row of m_a must be of the same size")
         ro = []
         for j in range(len(m_a[0])):
             el = 0
+            leb = len(m_b[0])
             for k in range(len(m_b)):
-                if not m_a[i][k]:
-                    raise TypeError("each row of m_a must be of the same size")
-                if not m_b[k][j]:
+                if len(m_b[k]) != leb:
                     raise TypeError("each row of m_b must be of the same size")
-                if type(m_a[i][k]) is not int or type(m_a[i][k]) is not float:
+                if type(m_a[i][k]) is not int and type(m_a[i][k]) is not float:
                     raise TypeError("m_a should contain only integers or floats")
-                if type(m_b[k][j]) is not int or type(m_b[k][j]) is not float:
+                if type(m_b[k][j]) is not int anu type(m_b[k][j]) is not float:
                     raise TypeError("m_b should contain only integers or floats")
                 el += m_a[i][k] * m_b[k][j]
             ro.append(el)
