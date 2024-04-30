b1b229937b48c96e7f9e4dfbc2f82da8a04ff8c6
AceDemo1 <lawal641@gmail.com>
2024-04-30 09:36:30 -0700

    mul matirx

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -28,34 +28,31 @@ def matrix_mul(m_a, m_b):
 
     if type(m_a) is not list:
        raise TypeError('m_a must be a list')
-    for i in range(len(m_a)):
-        if type(m_a[i]) is not list:
-            raise TypeError('m_a must be a list of lists')
     if type(m_b) is not list:
        raise TypeError('m_b must be a list')
-    for k in range(len(m_b)):
-        if type(m_b[k]) is not list:
-            raise TypeError('m_b must be a list of lists')
     if not m_a or not m_a[0]:
         raise ValueError("m_a can't be empty")
     if not m_b or not m_b[0]:
         raise ValueError("m_b can't be empty")
+    
 
     re = []
-    lea = len(m_a[0])
     for i in range(len(m_a)):
-        if len(m_a[i]) != lea:
-            raise TypeError("each row of m_a must be of the same size")
+        if type(m_a[i]) is not list:
+            raise TypeError('m_a must be a list of lists')
         ro = []
-        for j in range(len(m_b[0])):
+        for j in range(len(m_a[0])):
             el = 0
-            leb = len(m_b[0])
             for k in range(len(m_b)):
-                if len(m_b[k]) != leb:
+                if type(m_b[k]) is not list:
+                    raise TypeError('m_b must be a list of lists')
+                if not m_a[i][k]:
+                    raise TypeError("each row of m_a must be of the same size")
+                if not m_b[k][j]:
                     raise TypeError("each row of m_b must be of the same size")
                 if type(m_a[i][k]) is not int and type(m_a[i][k]) is not float:
                     raise TypeError("m_a should contain only integers or floats")
-                if type(m_b[k][j]) is not int and type(m_b[k][j]) is not float:
+                if type(m_b[k][j]) is not int and type(m_a[i][k]) is not float:
                     raise TypeError("m_b should contain only integers or floats")
                 el += m_a[i][k] * m_b[k][j]
             ro.append(el)
