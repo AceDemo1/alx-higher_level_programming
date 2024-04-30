753be15290ca00afc4441cec4982db3f74438a16
AceDemo1 <lawal641@gmail.com>
2024-04-30 08:01:28 -0700

    class rec width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,13 +5,16 @@
 class Rectangle:
     """ Define rectangle class """
     def __init__(self, width=0, height=0):
+        """ Instantation """
         self.width = width
         self.height = height
 
     def width(self):
+        """ Width """
         return self.__width
 
     def width(self, value):
+        """ Width setter """
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
@@ -19,9 +22,11 @@ class Rectangle:
         self.__width = value
 
      def height(self):
+         """ Height """
         return self.__height
 
     def height(self, value):
+        """ Height setter """
         if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
