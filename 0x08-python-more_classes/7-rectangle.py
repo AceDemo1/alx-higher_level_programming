ac0dabd8e2c5a1eabf35efbfa930268b2e3da866
AceDemo1 <lawal641@gmail.com>
2024-05-01 01:46:24 -0700

    rec update

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -55,8 +55,8 @@ class Rectangle:
         """ str """
         if self.__width == 0 or self.__height == 0:
             return ''
-        return (str(Rectangle.print_symbol) * self.__width + '\n') \
-            * (self.__height - 1) + str(Rectangle.print_symbol) * self.__width
+        return (str(self.print_symbol) * self.__width + '\n') \
+            * (self.__height - 1) + str(self.print_symbol) * self.__width
 
     def __repr__(self):
         """ repr """
