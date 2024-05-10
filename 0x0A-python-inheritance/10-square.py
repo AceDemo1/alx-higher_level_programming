1ba5165b5724a8d8e277748e9ef8562b0a363286
AceDemo1 <lawal641@gmail.com>
2024-05-10 15:59:43 -0700

    inherintance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+i = __import__(9-rectangle.py).Rectangle
+""" square class """
+
+
+class Square(Rectangle):
+    """ define square class """
+
+    def __init__(self, size):
+        """ validate and make size private """
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """ area """
+        return self.__size ** 2
