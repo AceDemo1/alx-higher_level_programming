8afa1a5302738e9c0ecec954211582dca0f3a712
AceDemo1 <lawal641@gmail.com>
2024-05-10 17:05:16 -0700

    inherintance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" square class """
+i = __import__("9-rectangle").Rectangle
+
+
+class Square(i):
+    """ define square class """
+
+    def __init__(self, size):
+        """ validate and make size private """
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ print """
+        return "f[Square] {self.__width}/{self.__height}"
