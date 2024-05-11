a34ca7de777ac06eabc27e479d31933cdba94cc4
AceDemo1 <lawal641@gmail.com>
2024-05-10 18:46:35 -0700

    set attr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 def add_attribute(ob, attr, va):
     """define func"""
 
-    if not isinstance(ob, type) and hasattr(ob, '__slot__'):
+    if not hasattr(ob, '__dict__'):
         raise TypeError("can't add new attribute")
     else:
         setattr(ob, attr, va)
