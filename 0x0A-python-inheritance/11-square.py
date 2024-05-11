9241f6adcade4cca9e8af43a910b0c51588d566c
AceDemo1 <lawal641@gmail.com>
2024-05-10 17:30:24 -0700

    inherintance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,6 +10,7 @@ class Square(i):
         """ validate and make size private """
         self.integer_validator('size', size)
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
         """ print """
