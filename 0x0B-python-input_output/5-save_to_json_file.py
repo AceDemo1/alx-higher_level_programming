baf2ac45922a23e63ddc5744c1c4a367e97b1a49
AceDemo1 <lawal641@gmail.com>
2024-05-13 01:59:28 -0700

    save to json fils

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""save to json file"""
+import json
+
+
+
+def save_to_json_file(my_obj, filename):
+    """define func"""
+    with open(filename, "w") as f:
+        f.write(json.dumps(my_obj))
