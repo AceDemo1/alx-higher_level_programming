f9c22e1d54c5c4d807f6de0d73f0f9bb4a72a703
AceDemo1 <lawal641@gmail.com>
2024-05-13 19:22:47 -0700

    adds args to a list an save as json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+i = __import__("5-save_to_json_file").save_to_json_file
+j = __import__("6-load_from_json_file").load_from_json_file
+k = __import__("sys").argv
+
+l = []
+l.extend(k[1:])
+j(l, add_item.json)
+
