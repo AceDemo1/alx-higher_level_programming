aa805bf8b473a7a4ab180f4b4d68a927bf7aaf0a
AceDemo1 <lawal641@gmail.com>
2024-05-14 08:39:28 -0700

    student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dict"""
+        if attrs is not None:
+            k = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    k[i] = self.__dict__[i]
+            return k
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """reload from json"""
+        for i, j in json.items():
+            self.i = i[j]
