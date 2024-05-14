447191723f0c19c6878efbcdca478096e278e6c0
AceDemo1 <lawal641@gmail.com>
2024-05-14 05:04:32 -0700

    student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dict"""
+        if attr is not None:
+        k = {}
+        for i in attrs:
+            if i in self.__dict__:
+            k[i] = self.__dict__
+            return k
+        return self.__dict__  
