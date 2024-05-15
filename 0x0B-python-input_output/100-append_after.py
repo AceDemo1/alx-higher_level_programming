4a78e5485bb9e468986888a76d957e82da670d0d
AceDemo1 <lawal641@gmail.com>
2024-05-15 05:29:20 -0700

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""append after"""
+
+
 def append_after(filename="", search_string="", new_string=""):
+    """define func"""
     with open(filename, "r") as f:
         k = []
         while True:
