71c8c868db5507561f78a47cc7d505260d4a6753
AceDemo1 <lawal641@gmail.com>
2024-06-30 08:44:14 -0700

    create and insert

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
--- create
+-- create and insert
 CREATE IF NOT EXISTS second_table (
 	id INT
 	name VARCHAR(256)
 	score INT
 )
-INSERT INTO id, name, score VALUES (1 , 'John', 10)
+INSERT INTO (id, name, score) VALUES (1 , 'John', 10)
 (2, 'Alex', 3)
 (3, 'Bob', 14)
 (4, 'George', 8)
