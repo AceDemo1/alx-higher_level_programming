93af818e9ed782f527e14e6f9536eeeaeaec9650
AceDemo1 <lawal641@gmail.com>
2024-07-03 16:25:49 -0700

    creates database and table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- creates database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS btn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	state_id INT NOT NULL FOREIGN KEY REFERENCES state(id),
 	name VARCHAR(256) NOT NULL
 	);
