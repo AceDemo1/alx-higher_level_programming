c574977c11826fe6bb41c2b1a49d86ce402c7c0f
AceDemo1 <lawal641@gmail.com>
2024-07-04 09:41:39 -0700

    tv show

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
--- import dump file 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+-- import dump file
+SELECT tv_genres.name AS genres, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
-
