634e35ebc92abc0015e9070c9c6f88a8cb19cf92
AceDemo1 <lawal641@gmail.com>
2024-07-04 18:56:37 -0700

    tv show

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,9 @@
 -- import dump file
-SELECT tv_genres.name AS name
+SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title != 'Dexter'
+GROUP BY tv_genres.name
 ORDER BY tv_genres.name;
 
