aebd560bba3e7324577a89741d6502f4b6e1daed
AceDemo1 <lawal641@gmail.com>
2024-07-06 07:49:22 -0700

    tv show

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,9 @@
 -- import dump file
-SELECT tv_show.title
-FROM tv_show
-WHERE tv_show.id NOT IN (
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
 	SELECT tv_show_genres.show_id
 	FROM tv_show_genres
 	JOIN tv_genres ON tv_show_genres.genres_id = tv_genres.id
 	WHERE name = Comedy)
-ORDER BY tv_show.title
+ORDER BY tv_shows.title
