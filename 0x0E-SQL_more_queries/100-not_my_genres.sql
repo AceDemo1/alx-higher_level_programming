6e46450c7facfbae7efc98caa1e9002c3a8db3eb
AceDemo1 <lawal641@gmail.com>
2024-07-06 06:46:54 -0700

    tv show

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,11 @@ SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title != 'Dexter'
+WHERE tv_genres.id NOT IN (
+	SELECT genre_id
+	FROM tv_show_genres
+ 	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    	WHERE tv_shows.title = 'Dexter'
+)
 GROUP BY tv_genres.name
 ORDER BY tv_genres.name;
-
