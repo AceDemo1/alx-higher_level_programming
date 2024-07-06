d7adce0d0ddb5f59603b597a8ebd7bf589f0c04d
AceDemo1 <lawal641@gmail.com>
2024-07-06 07:04:33 -0700

    tv show

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,5 +9,5 @@ WHERE tv_genres.id NOT IN (
  	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
     	WHERE tv_shows.title = 'Dexter'
 )
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
 ORDER BY tv_genres.name;
