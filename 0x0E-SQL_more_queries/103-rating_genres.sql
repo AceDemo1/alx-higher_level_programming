52dac8646b044711e7ba3320054f6b327e90e0e7
AceDemo1 <lawal641@gmail.com>
2024-07-06 08:55:54 -0700

    tv show

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- import dump file
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-JOIN tv_show_genres ON tv_shows_genres.id = tv_genres.id
+JOIN tv_show_genres ON tv_show_genres.id = tv_genres.id
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC
