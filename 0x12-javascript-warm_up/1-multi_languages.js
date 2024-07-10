e1d5832fd6336f6a805ef0c85e0c710ae6cbedaf
AceDemo1 <lawal641@gmai.com>
2024-07-10 06:42:38 -0700

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log('#!/usr/bin/node')
-console.log('Python is cool')
-console.log('JavaScript is amazing')
+console.log('#!/usr/bin/node');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
