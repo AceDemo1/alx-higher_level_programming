3ae9ca0706b0858eb75b25321c0a8a3114e50b14
AceDemo1 <lawal641@gmai.com>
2024-07-11 12:27:39 -0700

    isNaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const i = parseInt(process.argv[2])
-console.log(!isN(i) ? 'My number: ' + i : 'Not a number')
+const i = parseInt(process.argv[2]);
+console.log(!isN(i) ? 'My number: ' + i : 'Not a number');
