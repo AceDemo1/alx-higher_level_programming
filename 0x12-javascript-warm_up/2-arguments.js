65df792a0cfc2f0f3a3882d73c5977ff2349660a
AceDemo1 <lawal641@gmail.com>
2024-07-11 08:08:27 -0700

    argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const i = process.argv.length;
 if (i === 2) {
-	console.log('No argument');
+  console.log('No argument');
 }
 else if (i === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 }
 else if (i > 3) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
