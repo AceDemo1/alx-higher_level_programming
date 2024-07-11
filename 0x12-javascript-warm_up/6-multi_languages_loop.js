356ee5d010f5e8b0ab3a32f47b0c64fc1a6b0f49
AceDemo1 <lawal641@gmai.com>
2024-07-11 16:25:43 -0700

    for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-i = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+let i = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 for (let j = 0; j < i.length; j++) {
   console.log(i[j])
 }
