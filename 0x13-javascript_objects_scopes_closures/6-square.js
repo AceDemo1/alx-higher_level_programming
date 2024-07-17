415e7dea8f5cee00ab99d572a22dda36faa9bd50
AceDemo1 <lawal641@gmai.com>
2024-07-16 19:21:48 -0700

    class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle')
 class Square extends Rectangle {
-  constructor (size) {
+  constructor (size, size) {
     super();
   }
 }
+module.exports = Square;
