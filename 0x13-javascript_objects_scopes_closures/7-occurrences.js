c5c774d7aa8d4a0e667a95cd2d82f6a3afc67fcf
AceDemo1 <lawal641@gmai.com>
2024-07-16 19:55:33 -0700

    search element

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let i = 0;
-  for (let j = 0; j < list.lenght; j++) {
+  for (let j = 0; j < list.length; j++) {
     if (searchElement === list[j]) {
       i++;
     }
