b6cbecebd40d738e5b16a2688dbc51f7542b49c7
AceDemo1 <lawal641@gmai.com>
2024-07-17 18:00:40 -0700

    invert dic

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const i = require('./101-data').dict;
+k = {};
+for (const i in dict) {
+  const j = dict[i];
+  if (!k[j]) {
+    k[j] = [];
+  }
+  k[j][k[j].length] = i;
+}
+return k;
