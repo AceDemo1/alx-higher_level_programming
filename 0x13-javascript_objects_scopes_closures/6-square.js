8fb3a821dac84bbf72c653f7195ad0a59ec7ab4a
AceDemo1 <lawal641@gmai.com>
2024-07-17 18:44:46 -0700

    class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const Square5 = require('./5-square');
 
-class Square extends Square5 {
-
-  charPrint(c) {
+class Squarei extends Square5 {
+  charPrint (c) {
     for (let i = 0; i < this.height; i++) {
       console.log(c ? c.repeat(this.width) : 'X'.repeat(this.width));
     }
