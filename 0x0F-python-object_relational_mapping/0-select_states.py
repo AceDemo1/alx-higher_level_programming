1bc52c53bb77bf46b4c563cbd2c9efdf9fe63de2
AceDemo1 <lawal641@gmail.com>
2024-07-21 23:54:59 -0700

    mysqldb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,25 +4,26 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    def conc(na, ur, pa, db):
-        """connection"""
-        conn = MySQLdb.connect(
-            host=na, user=ur,
-            passwd=pa, database=db, port=3306)
-        return conn
+def conc(na, ur, pa, db):
+    """connection"""
+    conn = MySQLdb.connect(
+        host=na, user=ur,
+        passwd=pa, db=db, port=3306
+    )
+    return conn
 
-    def ex(conn, q):
-        """execute"""
-        cu = conn.cursor()
-        cu.execute(q)
-        r = cu.fetchall()
-        cu.close()
-        return r
+def ex(conn, q):
+    """execute"""
+    cu = conn.cursor()
+    cu.execute(q)
+    r = cu.fetchall()
+    cu.close()
+    return r
 
-    conne = conc("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
-    q = 'SELECT * FROM states'
-    rows = ex(conne, q)
-    for i in rows:
-        print(i)
-    conne.close()    
+conne = conc("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+q = 'SELECT * FROM states'
+rows = ex(conne, q)
+for i in rows:
+    print(i)
+conne.close()
+
