745ee6977a3f2cdf04a762d7e67a390af9a6c497
AceDemo1 <lawal641@gmail.com>
2024-07-23 00:52:05 -0700

    mysqldb

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""lists all states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    def conc(na, ur, pa, db):
+        """connection"""
+        conn = MySQLdb.connect(
+            host=na, user=ur,
+            passwd=pa, database=db, port=3306)
+        return conn
+
+    def ex(conn, q, pa):
+        """execute"""
+        cu = conn.cursor()
+        cu.execute(q)
+        r = cu.fetchall()
+        cu.close()
+        return r
+
+    conne = conc("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    q = (
+            "SELECT * FROM states WHERE name LIKE BINARY '{}'"
+            "ORDER BY states.id")
+    rows = ex(conne, q, (sys.argv[4],))
+    for i in rows:
+        print(i)
+    conne.close()
