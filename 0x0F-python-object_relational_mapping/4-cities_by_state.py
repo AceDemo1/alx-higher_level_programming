32537c4cc9822519df858e3cd256b3cba738777f
AceDemo1 <lawal641@gmail.com>
2024-07-23 01:21:10 -0700

    mysqldb

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,9 @@ if __name__ == "__main__":
         return r
 
     conne = conc("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
-    q = 'SELECT * FROM cities ORDER BY cities.id'
+    q = (
+            'SELECT * FROM cities WHERE state_id = cities.id'
+            'ORDER BY cities.id')
     rows = ex(conne, q)
     for i in rows:
         print(i)
