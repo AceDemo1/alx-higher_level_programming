5e3c7fbf4a01cf6b6aa813761b65e2894ea6b6bc
AceDemo1 <lawal641@gmail.com>
2024-07-23 03:28:41 -0700

    mysqldb

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""lists all states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    def conc(na, ur, pa, db):
+        """connection"""
+        conn = MySQLdb.connect(
+            host=na, user=ur,
+            passwd=pa, database=db, port=3306)
+        return conn
+
+    def ex(conn, q, pa):
+        """execute"""
+        cu = conn.cursor()
+        cu.execute(q, pa)
+        r = cu.fetchall()
+        cu.close()
+        return r
+
+    conne = conc("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    q = (
+            "SELECT cities.name JOIN states "
+            "ON cities.state_id=states.id WHERE states.name=%s"
+            "ORDER BY cities.id")
+    rows = ex(conne, q, (sys.argv[4],))
+    for i in rows:
+        print(i)
+    conne.close()
