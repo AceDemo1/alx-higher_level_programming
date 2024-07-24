dfae360b539ad1ece8fe8403f0635cfbb3dffa31
AceDemo1 <lawal641@gmail.com>
2024-07-23 17:06:35 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     sess = sessionmaker(bind=k)
     m = sess()
     row = m.query(State).first()
-    print(row.id, row.name, sep=': ')
+    print((row.id, row.name, sep=': ') if row else 'Nothing')
     m.close()
