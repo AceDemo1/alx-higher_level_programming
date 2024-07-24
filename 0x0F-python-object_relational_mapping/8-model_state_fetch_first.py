a95e77cc8ceaede05d5d95f9c79fc21c532188f7
AceDemo1 <lawal641@gmail.com>
2024-07-23 17:02:18 -0700

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+import sys
+from sqlalchemy import create_engine
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    k = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                      .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(k)
+    sess = sessionmaker(bind=k)
+    m = sess()
+    row = m.query(State).all()
+    print(row[0].id, j[0].name, sep=': ')
+    m.close()
