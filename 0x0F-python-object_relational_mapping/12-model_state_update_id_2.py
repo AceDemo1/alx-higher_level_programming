cde82f780febd3d20fa8042e3d3fc54111942d70
AceDemo1 <lawal641@gmail.com>
2024-07-24 08:25:09 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -4,31 +4,16 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
-        return
-
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create engine and session
-    engine = create_engine(f"mysql+mysqldb://{mysql_user}:{mysql_password}@localhost:3306/{database_name}")
+if __name__ == "__main__":
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Update State with id = 2
-    state = session.query(State).filter_by(id=2).first()
+    # Fetch and print state with id = 2
+    state = session.query(State).filter(State.id == 2).first()
     if state:
+        print(f"Original state name: {state.name}")
         state.name = "New Mexico"
         session.commit()
-    else:
-        print("State with id 2 not found.")
-
-    # Close session
     session.close()
-
-if __name__ == "__main__":
-    main()
 
