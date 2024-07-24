0ac8e65ceea69e3a8d6d84410d6bad36a69fa15e
AceDemo1 <lawal641@gmail.com>
2024-07-24 14:34:16 -0700

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+import sys
+from sqlalchemy import create_engine
+from model_state import State, Base, City
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    k = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                      .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(k)
+    sess = sessionmaker(bind=k)
+    m = sess()
+    row = m.query(State.name, City.id, City.name)\
+            .filter(State.id == City.state_id)all()
+    for i, j, k in row:
+        print(i + ': (' + j + ') ' + k)
+    m.close()
