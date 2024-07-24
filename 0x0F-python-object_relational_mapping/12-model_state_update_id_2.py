65d7edfb997e602570123e1ff27492fd03513a89
AceDemo1 <lawal641@gmail.com>
2024-07-24 08:54:26 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Change the name of the State object with id = 2 to New Mexico in the database."""
-
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 import sys
 from sqlalchemy import create_engine
+from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == "__main__":
-    # Connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-    # Create a configured "Session" class and a Session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query for the State with id = 2
-    state = session.query(State).filter(State.id == 2).first()
-    if state:
-        state.name = "New Mexico"
-        session.commit()
-    session.close()
+if __name__ == '__main__':
+    k = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                      .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(k)
+    sess = sessionmaker(bind=k)
+    m = sess()
+    chan = m.query(State).filter(State.id == 2).first()
+    if chan:
+        chan.name = 'New Mexico'
+        m.commit()
+    m.close()
