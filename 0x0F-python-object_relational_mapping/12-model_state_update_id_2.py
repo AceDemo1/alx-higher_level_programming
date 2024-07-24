527d170edd197ba67d455173373ddce5eb0366c0
AceDemo1 <lawal641@gmail.com>
2024-07-24 04:56:48 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-"""Script that changes the name of a State object from the database
- hbtn_0e_6_usa"""
+""" Prints the State object with the name passed as argument from the database,
+or updates the State object with id 2 to 'New Mexico'.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    Session.configure(bind=engine)
     session = Session()
-    session.query(State).filter(State.id == 2).\
-        update({State.name: 'New Mexico'}, synchronize_session=False)
-    session.commit()
+
+    # Update state with id 2
+    state_id = 2
+    new_name = 'New Mexico'
+    state_to_update = session.query(State).filter_by(id=state_id).first()
+    if state_to_update:
+        state_to_update.name = new_name
+        session.commit()
+        print(f"State updated: {state_to_update.id}: {state_to_update.name}")
+    else:
+        print("State not found.")
+
+    # Alternatively, find a state by name
+    if len(sys.argv) > 4:
+        state_name = sys.argv[4]
+        state = session.query(State).filter_by(name=state_name).first()
+        if state:
+            print(f"{state.id}: {state.name}")
+        else:
+            print("Not found")
+    
     session.close()
+
