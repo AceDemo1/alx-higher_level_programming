f80d16d2ff228f0a2842cd64a1faafdf35f806c2
AceDemo1 <lawal641@gmail.com>
2024-07-24 06:55:37 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 """
-Script that changes the name of a State object in the database.
+Contains State class and Base, an instance of declarative_base()
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State, Base  # Ensure 'Base' is imported correctly
 
-if __name__ == "__main__":
-    # Create an engine and connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    
-    # Bind the engine to the Base's metadata
-    Base.metadata.create_all(engine)
-    
+if __name__ == '__main__':
+    # Create engine and connect to the MySQL server
+    k = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                      .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # Create all tables in the database
+    Base.metadata.create_all(k)
+
+    # Create a configured "Session" class
+    l = sessionmaker(bind=k)
+
     # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    
-    # Query for the state with id = 2
-    state = session.query(State).filter_by(id=2).first()
-    if state:
-        state.name = "New Mexico"
-        session.commit()
-    
+    m = l()
+
+    # Query all State objects
+    i = m.query(State).all()  # Use the session instance 'm'
+
+    # Print the states
+    for j in i:
+        print(f"{j.id}: {j.name}")
+
     # Close the session
-    session.close()
+    m.close()
+
