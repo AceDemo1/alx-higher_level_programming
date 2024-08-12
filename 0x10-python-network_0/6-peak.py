50ad55a2a159894cdf965f20a900d6edf928d345
AceDemo1 <lawal641@gmail.com>
2024-08-12 07:38:48 -0700

    peak value

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,13 +6,10 @@ def find_peak(list_of_integers):
     """jj"""
     if not list_of_integers:
         return None
-
     low = 0
     high = len(list_of_integers) - 1
-
     while low < high:
         mid = (low + high) // 2
-        
         # Check if the mid element is greater than the next element
         if list_of_integers[mid] > list_of_integers[mid + 1]:
             # Peak is on the left side or it is the mid element
@@ -20,5 +17,4 @@ def find_peak(list_of_integers):
         else:
             # Peak is on the right side
             low = mid + 1
-
     return list_of_integers[low]
