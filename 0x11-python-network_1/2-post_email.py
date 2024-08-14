542452a08abe08592bf2075d9bd7810f665990c0
AceDemo1 <lawal641@gmail.com>
2024-08-13 19:28:50 -0700

    post urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,6 +7,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
-    with urllib.request.urlopen(url) as f:
+    dat = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+    with urllib.request.urlopen(url, data=dat) as f:
         print(f.read().decode('utf-8'))
