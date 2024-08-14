003916af7ef7b1106a9ec69a3a2ca871888db88b
AceDemo1 <lawal641@gmail.com>
2024-08-13 20:05:01 -0700

    header request

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""get"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    body = request.get(url)
+    print(body.headers.get('X-Request-Id'))
