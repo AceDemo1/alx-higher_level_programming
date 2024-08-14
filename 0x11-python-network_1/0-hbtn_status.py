18bb8382edc535fa4875f3d79586eced1d03dbf8
AceDemo1 <lawal641@gmail.com>
2024-08-13 18:15:23 -0700

    get urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
+    body = f.read()
+    print('Body reponse:')
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
