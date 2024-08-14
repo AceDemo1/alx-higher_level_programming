062dba9226bfa61660be0036b42bd180fb462480
AceDemo1 <lawal641@gmail.com>
2024-08-14 05:16:39 -0700

    error requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""url"""
+import requests
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    body = requests.get(url)
+    if body.status_code >= 400:
+        print(f'Error code: {body.statues_code}')
+    else:
+        print(body.text)
