5501ad65b3298eed513b9431f3740c1b80452237
AceDemo1 <lawal641@gmail.com>
2024-08-13 19:57:20 -0700

    get request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 
 body = requests.get('https://alx-intranet.hbtn.io/status')
 print('Body response:')
-print(f"\t- type: {type(body)}")
-print(f"\t- content: {body}")
+print(f"\t- type: {type(body.text)}")
+print(f"\t- content: {body.text}")
