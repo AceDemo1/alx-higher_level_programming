73e6ac16c36d4328f5793837fa6b1b45c2fd99b9
AceDemo1 <lawal641@gmail.com>
2024-08-13 19:45:53 -0700

    error urllib

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """url"""
 import urllib.request
-import urllib.parse
+import urllib.error
 import sys
 
 
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as f:
-        print(f.read().decode('utf-8'))
+            print(f.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f'Error code: {e.code})
+        print(f'Error code: {e.code}')
