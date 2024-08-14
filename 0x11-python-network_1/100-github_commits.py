bcef91a21d9bc24e714a13543c226ae1b1a36317
AceDemo1 <lawal641@gmail.com>
2024-08-14 10:47:51 -0700

    commits github requests

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    body = requests.get(f"https://api.github.com/{sys.argv[1]}/{sys.argv[2]}")
+    body = requests.get(f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits")
     json = body.json()
     for i in json[:10]:
         print(f"{i.get('sha')}: {i.get('commits').get('author').get('name')}")
