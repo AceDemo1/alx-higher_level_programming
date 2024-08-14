67f2e526bb2bae086d7067acc40b758af364b813
AceDemo1 <lawal641@gmail.com>
2024-08-14 07:45:43 -0700

    json requests

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     body = requests.post(url, data={'q': letter})
     contype = body.headers.get('Content-Type')
     try:
-        if body.json():
-            print(f"[{body.json().get('id')}] {body.json().get('name')}") 
+        json = body.json()
+        if json:
+            print(f"[{json.get('id')}] {json.get('name')}")
         else:
             print('No result')
     except ValueError:
         print('Not a valid JSON')
-
