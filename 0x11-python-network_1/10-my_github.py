aebb13b0702d836565f5dddf6dccec39ac6288b2
AceDemo1 <lawal641@gmail.com>
2024-08-14 08:04:42 -0700

    github auth requests

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,14 +5,6 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    body = requests.post(url, data={'q': letter})
-    try:
-        json = body.json()
-        if json:
-            print(f"[{json.get('id')}] {json.get('name')}")
-        else:
-            print('No result')
-    except ValueError:
-        print('Not a valid JSON')
+    url = 'https://api.github.com/user'
+    body = requests.get(url, auth=sys.argv[1], sys.argv[2])
+    print(f"{body.json().get('id')}")
