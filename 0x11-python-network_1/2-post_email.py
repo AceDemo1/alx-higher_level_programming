e9fbfbc8e4e2d438ff9b629d3537f8a171200c78
AceDemo1 <lawal641@gmail.com>
2024-08-13 19:21:25 -0700

    post urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """get"""
 import urllib.request
+import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = urllib.parse.urlencode({'email': sys.argv[2]}.encode('utf-8'))2-post_email.py
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8'))
     with urllib.request.urlopen(url) as f:
         print(f.read().decode('utf-8'))
